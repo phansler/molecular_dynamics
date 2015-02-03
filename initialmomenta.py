f368b146fea664c6371ca2aa1f91010d19f48c67
Delft User06 Account <delft06@bps1240-19.pa.msu.edu>
2015-02-03 18:03:58 -0500

    first version

---
+++ initialmomenta.py
@@ -0,0 +1,36 @@
+#Initial momenta module
+
+import random as rn
+
+def im(N):
+	ICll = []
+	for i in range(N):
+		ICL = []
+		for dr in range(3):
+			ICL.append(rn.normalvariate(0,1))
+		ICll.append(ICL)
+
+	trx,trY,trz = 0,0,0
+	for i in range(len(ICll)):
+		trx = trx + ICll[i][0]
+		trY = trY + ICll[i][1]
+		trz = trz + ICll[i][2]
+
+	mnx = trx/len(ICll)
+	mny = trY/len(ICll)
+	mnz = trz/len(ICll)
+
+	for i in range(len(ICll)):
+		ICll[i][0] = ICll[i][0] - mnx
+		ICll[i][1] = ICll[i][1] - mny
+		ICll[i][2] = ICll[i][2] - mnz
+
+	trx2,trY2,trz2 = 0,0,0
+	for i in range(len(ICll)):
+		trx2 = trx2 + ICll[i][0]
+		trY2 = trY2 + ICll[i][1]
+		trz2 = trz2 + ICll[i][2]
+
+	#print trx2,trY2,trz2, ICll
+
+	return ICll
