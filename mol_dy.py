07897eebf14cf54987ef4c0864993957a5881645
Delft User06 Account <delft06@bps1240-19.pa.msu.edu>
2015-02-03 18:07:00 -0500

    last version before making overview file

--- mol_dy.py
+++ mol_dy.py
@@ -3,6 +3,7 @@
 import numpy as np
 import random as rn
 import math
+import forcemodule as fm
 
 # Structure
 
@@ -11,8 +12,9 @@ mom = []
 F = [[],[],[]]
 dt = 1
 l = 1.
-N=32.
+N=21
 gs = l*N**-(1./3.)
+lpnum = 100
 
 #print range(N)
 
@@ -24,21 +26,55 @@ for z in range(3):
 	ICl = []	
 	for i in range(N):
 		ICl.append(rn.normalvariate(0,1))
-	ICll.append(ICl)
-	
+		#print ICl
+		ICll.append(ICl)
+'''
+ICll = []
+for i in range(N):
+	ICL = []
+	for dr in range(3):
+		ICL.append(rn.normalvariate(0,1))
+	ICll.append(ICL)
+
+trx,trY,trz = 0,0,0
+for i in range(len(ICll)):
+	trx = trx + ICll[i][0]
+	trY = trY + ICll[i][1]
+	trz = trz + ICll[i][2]
+
+mnx = trx/len(ICll)
+mny = trY/len(ICll)
+mnz = trz/len(ICll)
+
+for i in range(len(ICll)):
+	ICll[i][0] = ICll[i][0] - mnx
+	ICll[i][1] = ICll[i][1] - mny
+	ICll[i][2] = ICll[i][2] - mnz
+
+trx2,trY2,trz2 = 0,0,0
+for i in range(len(ICll)):
+	trx2 = trx2 + ICll[i][0]
+	trY2 = trY2 + ICll[i][1]
+	trz2 = trz2 + ICll[i][2]
+
+#print trx2,trY2,trz2, ICll
 
 
+
+'''
 #plugging the initial conditions in the momenta lists
 for z in range(3):
 	mom.append(ICll[z])
+
+print mom
 
 
 for i in range(3):
 	tr = np.mean(mom[i])
 	mom[i] = [x - tr for x in mom[i]]
 	#print np.mean(mom[i])
-	
-'''
+'''	
+
 	
 #plugging the initial positions for x
 
@@ -108,10 +144,21 @@ for idx,z in enumerate(zpl):
 			part = [partx,party,partz]
 			partl.append(part)
 		
-print partl
+#print partl,len(partl)
 
-#test with the "known" formula
+#Creating the F0 list
+
+rempart = len(partl)
+
+forces = []
+for i in range(rempart):
+	forces.append([0,0,0])
+
+
+
 '''
+#test with the "known" formula
+
 part_t = []
 for k in range(ynum):
 	for j in range(ynum):
@@ -123,7 +170,18 @@ for k in range(ynum):
 			part_t.append(part)
 			
 print part_t
-'''
-#onzin
 
-# Module to find 
+ '''
+# Moving particles
+
+ 
+#print len(partl), len(partl[0]),range(3),len(range(len(partl))),len(mom)
+for lp in range(lpnum):
+	print partl[0]
+	forces = fm.calc_forces(partl,forces)
+	for i in range(len(partl)):
+		for z in range(3):
+			partl[i][z] = partl[i][z] + ICll[i][z]*dt + (forces[i][z]/2)*dt**2
+			ICll[i][z] = ICll[i][z] + forces[i][z]*dt	
+
+
