69decd2d154f2f0b1b14eb29902a8097944cd31c
Delft User06 Account <delft06@bps1240-19.pa.msu.edu>
2015-02-03 19:12:37 -0500

    new initpos

--- mol_dy2.py
+++ mol_dy2.py
@@ -14,20 +14,27 @@ import initialpositionP as ipP
 # independent parameters
 dt = 1
 l = 1.
-N=21
+N=32
 gs = l*N**-(1./3.)
 lpnum = 100
+a=1
 
-momenta = im.im(N)
-partl,forces0,ynum,xnum = ipAD.ip(N,gs)
-part_t = ipP.ip(ynum,xnum,gs)
+ICll = im.im(N)
+partl,forces,ynum,xnum,znum = ipAD.ip(N,gs,l)
+partl = ipP.ip(N,a)
+
+print partl
 
 # Updating postion, making use of force calculator
 
+'''
 for lp in range(lpnum):
-	print partl[0]
+	#print partl[0]
 	forces = fm.calc_forces(partl,forces)
 	for i in range(len(partl)):
 		for z in range(3):
 			partl[i][z] = partl[i][z] + ICll[i][z]*dt + (forces[i][z]/2)*dt**2
 			ICll[i][z] = ICll[i][z] + forces[i][z]*dt
+ '''
+
+#print part_t,'=partt',len(part_t), partl,'=partl',len(partl)
