6838fe47d506715cb9ceb0c4912bf213b48cba9c
Delft User06 Account <delft06@bps1240-19.pa.msu.edu>
2015-02-06 12:38:37 -0500

    Renormalized

--- mol_dy2.py
+++ mol_dy2.py
@@ -16,11 +16,13 @@ import init_sys
 
 
 # independent parameters
-dt = 0.1
-N=32
-lpnum = 100
+dt = 0.0004
+N=864
+lpnum = 2000
 density = 0.85
 temp = 0.8
+Ttarg = 300
+Kb =  1
 
 
 # Loading initial conditions
@@ -29,7 +31,7 @@ pos, l = init_sys.init_pos(N, density)
 forces = init_sys.init_forc(N)
 pot = init_sys.init_pot(N)
 
-print N, 'N'
+
 
 
 
@@ -44,8 +46,24 @@ for lp in range(lpnum):
   forces, pot = fm.calc_forces(pos,forces,pot,l,[N])
   mom = mom + forces*0.5*dt
   Ken = np.sum(mom*mom*0.5, axis=1)
-  toten = sum(Ken) - sum(pot)
-  print toten, np.sum(mom)
+  toten = sum(Ken) + sum(pot)
+  if lp % 100 == 0:
+    lamda = np.sqrt((Ttarg*3.*(N-1.)*Kb)/(np.sum(Ken)*2.))
+#  print type(lamda), len(lamda), type(forces), type(mom), type(0.5*dt)
+    mom = mom*lamda
+    print lp, 'lp',lamda, 'lambda', np.sum(Ken), 'Ken'
+  
+  
+ 
+
+
+
+#  print sum(Ken), sum(pot)[0], toten[0]#, np.sum(mom)
+# Plotting the positions
+
+
+
+'''
   fig = pylab.figure()
   ax = Axes3D(fig) 
   ax.scatter(pos[:,0],pos[:,1],pos[:,2],c='b')
@@ -53,13 +71,4 @@ for lp in range(lpnum):
   ax.set_ylabel('Y Label')
   ax.set_zlabel('Z Label')
   plt.show()
-
-
-
-
-
-# Plotting the positions
-
-
-
-
+ '''
--- mol_dy2_tmp.py
+++ mol_dy2_tmp.py
@@ -16,13 +16,11 @@ import init_sys
 
 
 # independent parameters
-dt = 0.0004
-N=256
-lpnum = 2000
+dt = 0.004
+N=2048
+lpnum = 1000
 density = 0.85
 temp = 0.8
-Ttarg = 300
-Kb =  1
 
 
 # Loading initial conditions
@@ -31,7 +29,7 @@ pos, l = init_sys.init_pos(N, density)
 forces = init_sys.init_forc(N)
 pot = init_sys.init_pot(N)
 
-
+print N, 'N'
 
 
 
@@ -46,23 +44,8 @@ for lp in range(lpnum):
   forces, pot = fm.calc_forces(pos,forces,pot,l,[N])
   mom = mom + forces*0.5*dt
   Ken = np.sum(mom*mom*0.5, axis=1)
-  toten = sum(Ken) + sum(pot)
-  if lp % 10 == 0:
-    lamda = np.sqrt((Ttarg*3.*(N-1.)*Kb)/(np.sum(Ken)*2.))
-#  print type(lamda), len(lamda), type(forces), type(mom), type(0.5*dt)
-    mom = mom*lamda
-    print lp, 'lp',lamda, 'lambda', np.sum(Ken), 'Ken'
-  
-  
- 
-
-
-
-#  print sum(Ken), sum(pot)[0], toten[0]#, np.sum(mom)
-# Plotting the positions
-
-
-
+  toten = sum(Ken) - sum(pot)
+  print toten, np.sum(mom)
 '''
   fig = pylab.figure()
   ax = Axes3D(fig) 
@@ -72,3 +55,12 @@ for lp in range(lpnum):
   ax.set_zlabel('Z Label')
   plt.show()
  '''
+
+
+
+
+# Plotting the positions
+
+
+
+
